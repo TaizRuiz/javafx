3340d23422790eb075b493d00411c3897dbad4c3
TaizRuiz <dafnetaizruiz@gmail.com>
2024-01-03 15:37:16 -0500

    vbox

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/PrimaryController.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/PrimaryController.java
@@ -8,14 +8,18 @@ import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class PrimaryController implements Initializable {
 
-    String archivo = "C:\\Users\\jaesc\\OneDrive\\Escritorio\\javafx\\mavenproject1\\src\\main\\java\\com\\mycompany\\mavenproject1\\archivo.txt";
+    String archivo = "C:\\Users\\USUARIO\\OneDrive\\Escritorio\\javafx\\mavenproject1\\src\\main\\java\\com\\mycompany\\mavenproject1\\archivo.txt";
     @FXML
     private BorderPane mainContainer;
     @FXML
@@ -23,15 +27,19 @@ public class PrimaryController implements Initializable {
     private ArrayList<Cancion> canciones=new ArrayList();
     
     private Label id = new Label();
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
         canciones = inicializarArray(archivo);
         for(Cancion c: canciones){
             System.out.println(c.toString());
         }
-        id.setText("RadioXYZ Top 10");
-        mainContainer.setTop(id);
+       
+//        setStyles();
+        setTitle();
+        llenarVbox();
     }
     
     
@@ -63,7 +71,42 @@ public class PrimaryController implements Initializable {
         return listaC;
     }
     
+    public void llenarVbox(){
+        
+        for(Cancion can: canciones){
+            Label posA=new Label(String.valueOf(can.getPostActual()));
+            String image = can.getImagen();
+            String title = can.getTitulo();
+            String singer = can.getCantante();
+            HBox cajitaH = new HBox();
+//            cajitaH.setAlignment(Pos.CENTER_LEFT);
+            cajitaH.setSpacing(20);
+            VBox cajitaV = new VBox();
+//            cajitaV.setAlignment(Pos.CENTER_LEFT);
+//            SetNumberStyle(posA);
+            cajitaV.getChildren().add(new Label(title));
+            cajitaV.getChildren().add(new Label(singer));
+            cajitaH.getChildren().add(posA);
+            cajitaH.getChildren().add(new Label(image));
+            cajitaH.getChildren().add(cajitaV);
+            cancionesContainer.getChildren().add(cajitaH);
+        }
+    }
+
+  public void setTitle(){
+       id.setText("RadioXYZ Top 10");
+        mainContainer.setTop(id);   
+  }
     
-    
-  
+  public void setStyles(){
+      BorderPane.setAlignment(id, Pos.CENTER);
+      
+      cancionesContainer.setPrefHeight(400);
+      cancionesContainer.setPrefWidth(400);
+  }
+  public void SetNumberStyle(Label l){
+      HBox.setMargin(l,new Insets(8));
+       l.setStyle("-fx-font-size: 18;");
+       l.setAlignment(Pos.CENTER);
+  }
 }
